d60b93cab76237e556ef8b776b77e230718715b0
Manni Wood <manni.lee.wood@gmail.com>
2015-03-21 23:37:46 -0400

    Updates copyright; documents all connection settings

--- src/test/java/com/manniwood/cl4pg/v1/test/ds/pgsimple/PgSimpleInsertReturningTest.java
+++ src/test/java/com/manniwood/cl4pg/v1/test/ds/pgsimple/PgSimpleInsertReturningTest.java
@@ -1,7 +1,7 @@
 /*
 The MIT License (MIT)
 
-Copyright (t) 2014 Manni Wood
+Copyright (c) 2015 Manni Wood
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
